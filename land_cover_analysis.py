04dfd96c27cad92f9da5ab9d7933a942e2ca691f
Thijs <vdplasthijs@gmail.com>
2022-10-25 16:39:57 +0100

    Create raster masks from shp files

--- land_cover_analysis.py
+++ land_cover_analysis.py
@@ -1,5 +1,6 @@
 import os, sys, copy
 import numpy as np
+from numpy.core.multiarray import square
 import rasterio
 import xarray as xr
 import rioxarray as rxr
@@ -12,6 +13,7 @@ from geocube.api.core import make_geocube
 import gdal, osr
 import loadpaths
 
+path_dict = loadpaths.loadpaths()
 
 def assert_epsg(epsg, project_epsg=27700):
     if type(epsg) != int:
@@ -34,6 +36,11 @@ def load_tiff(tiff_file_path, datatype='np', verbose=0):
 
     return im 
 
+def get_all_tifs_from_dir(dirpath):
+    assert type(dirpath) == str
+    list_tifs = [os.path.join(dirpath, x) for x in os.listdir(dirpath) if x[-4:] == '.tif']
+    return list_tifs 
+
 def load_coords_from_geotiff(tiff_file_path):
     ## from https://stackoverflow.com/questions/50191648/gis-geotiff-gdal-python-how-to-get-coordinates-from-pixel and https://gis.stackexchange.com/questions/126467/determining-if-shapefile-and-raster-overlap-in-python-using-ogr-gdal
     raster = gdal.Open(tiff_file_path)
@@ -45,32 +52,56 @@ def load_coords_from_geotiff(tiff_file_path):
     n_pix_y = raster.RasterYSize
     x_right = x_left + n_pix_x * px_width
     y_bottom = y_top + n_pix_y * px_height
-    
-    square_coords = shp.geometry.Polygon(zip([x_left, x_right, x_right, x_left], [y_bottom, y_bottom, y_top, y_top]))
-    df_tile = gpd.GeoDataFrame(crs=f'epsg:{raster_epsg}', geometry=[square_coords])
-    df_tile = df_tile.assign(name=tiff_file_path.split('/')[-1].rstrip('.tif'))  # could assign index with index=[0] 
-    
-    # # print(x_left, x_right, y_bottom, y_top)
+
+     # # print(x_left, x_right, y_bottom, y_top)
     # # supposing x and y are your pixel coordinate this is how to get the coordinate in space:
     # pix_x = 1000
     # pix_y= 4000
     # posX = px_width * pix_x + x_rot * pix_y + x_left
     # posY = y_rot * pix_x + px_height * pix_y + y_top
-    # # shift to the center of the pixel
+        # # shift to the center of the pixel
     # posX += px_width / 2.0
     # posY += px_height / 2.0
     # # print(posX, posY)
+    
+    square_coords = shp.geometry.Polygon(zip([x_left, x_right, x_right, x_left], [y_bottom, y_bottom, y_top, y_top]))
+    name_tile = tiff_file_path.split('/')[-1].rstrip('.tif')
+    return square_coords, name_tile, raster_epsg
 
+def create_df_with_tiff_coords(tiff_paths, verbose=0):
+
+    if type(tiff_paths) == str:
+        tiff_paths = [tiff_paths] 
+
+    if verbose > 0:
+        print(f'Loading {len(tiff_paths)} tiff files')
+
+    list_coords, list_names = [], []
+    prev_epsg = None
+    for i_tile, tile_path in enumerate(tiff_paths):
+        if verbose > 1:
+            print(i_tile, tile_path)
+        square_coords, name_tile, raster_epsg = load_coords_from_geotiff(tiff_file_path=tile_path)
+        list_coords.append(square_coords)
+        list_names.append(name_tile)
+
+        if i_tile > 0:  # ensure epsg stays consistent 
+            assert raster_epsg == prev_epsg
+        prev_epsg = raster_epsg
+
+    df_tile = gpd.GeoDataFrame(pd.DataFrame({'name': list_names}),
+                                             crs=f'epsg:{raster_epsg}', geometry=list_coords)
+    
     return df_tile
 
-def load_geo_tiff(tiff_file_path, datatype='np', verbose=0):
-    ## image
-    im = load_tiff(tiff_file_path=tiff_file_path, datatype=datatype, verbose=verbose)
+# def load_geo_tiff(tiff_file_path, datatype='np', verbose=0):
+#     ## image
+#     im = load_tiff(tiff_file_path=tiff_file_path, datatype=datatype, verbose=verbose)
 
-    # coords 
-    df_tile = load_coords_from_geotiff(tiff_file_path=tiff_file_path)
+#     # coords 
+#     df_tile = load_coords_from_geotiff(tiff_file_path=tiff_file_path)
 
-    return im, df_tile
+#     return im, df_tile
 
 def load_pols(pol_path):
     df_pols = gpd.read_file(pol_path)
@@ -91,6 +122,16 @@ def load_landcover(pol_path, col_class_ind='LC_N_80', col_class_names='LC_D_80')
         dict_classes[ind] = lc_class  # create mapping 
         
     return df_lc, dict_classes  
+
+def get_lc_mapping_inds_names_dicts(pol_path=path_dict['lc_80s_path'], 
+                                    col_class_ind='LC_N_80', col_class_names='LC_D_80'):
+
+    _, dict_ind_to_name = load_landcover(pol_path=pol_path, col_class_ind=col_class_ind, 
+                                         col_class_names=col_class_names)
+
+    dict_name_to_ind = {v: k for k, v in dict_ind_to_name.items()}
+
+    return dict_ind_to_name, dict_name_to_ind
 
 def get_pols_for_tiles(df_pols, df_tiles):
     '''Assuming a df for tiles currently.. Could just use Polygon geometry instead'''
@@ -116,19 +157,49 @@ def get_pols_for_tiles(df_pols, df_tiles):
     return dict_pols
 
 def convert_shp_mask_to_raster(df_shp, col_name='LC_N_80',
-                                resolution=(0.125, -0.125),
+                                resolution=(-0.125, 0.125),
+                                interpolation=None,
                                 save_raster=False, filename='mask.tif',
-                                plot_raster=False):
-
+                                maskdir=None, plot_raster=False,
+                                verbose=0):
+    '''
+    interpolation:
+        - None: nothing done with missing data (turned into 0)
+        - 'nearest': using label of nearest pixels (takes bit of extra time)
+    '''
+    assert not np.isin(0, np.unique(df_shp[col_name])), '0 is already a class label, so cant be used for fill value'
+    assert len(resolution) == 2 and resolution[0] < 0 and resolution[1] > 0, 'resolution has unexpected size/values'
+    
     ## Convert shape to raster:
     cube = make_geocube(df_shp, measurements=[col_name],
-                    # like=ex_tile)  # use resolution of example tiff
-                    resolution=resolution)
+                        interpolate_na_method=interpolation,
+                        # like=ex_tile)  # use resolution of example tiff
+                        fill=0,
+                        resolution=resolution)
+
+    ## Decrease data size:
+    if verbose > 0:
+        print(f'Current data size cube is {cube.nbytes / 1e6} MB')
+    unique_labels = copy.deepcopy(np.unique(cube[col_name]))  # want to ensure these are not messed up 
+    assert np.nanmin(unique_labels) >=0 and np.nanmax(unique_labels) < 256, f'unexpectedly high number of labels. conversion to int8 wont work. Labels: {unique_labels}'
+    low_size_raster = cube[col_name].astype('uint8')  # goes from 0 to & incl 255
+    cube[col_name] = low_size_raster
+    new_unique_labels = np.unique(cube[col_name])
+    assert (unique_labels == new_unique_labels).all(), f'Old labels: {unique_labels}, new labels: {new_unique_labels}, comaprison: {(unique_labels == new_unique_labels)}'  # unique labels are sorted by default so this works as sanity check
+    if verbose > 0:
+        print(f'New cube data size is {cube.nbytes / 1e6} MB')
+
 
     if save_raster:
-        filepath = filename
+        assert type(filename) == str, 'filename must be string'
+        if filename[-4:] != '.tif':
+            filename = filename + '.tif'
+        if maskdir is None:  # use default path for mask files 
+            maskdir = path_dict['mask_path']
+        filepath = os.path.join(maskdir, filename)
         cube[col_name].rio.to_raster(filepath)
-        print(f'Saved to {filepath}')
+        if verbose > 0:
+            print(f'Saved to {filepath}')
 
     if plot_raster:
         cube[col_name].plot()
--- land_cover_visualisation.py
+++ land_cover_visualisation.py
@@ -1,6 +1,9 @@
 import os, sys
+import random, json
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors 
+import matplotlib.patches as mpatches
 from cycler import cycler
 import seaborn as sns
 import rasterio, rasterio.plot
@@ -8,7 +11,7 @@ import xarray as xr
 import rioxarray as rxr
 import pandas as pd
 import geopandas as gpd
-import loadpaths
+import land_cover_analysis as lca
 
 ## Set default settings.
 plt.rcParams['axes.prop_cycle'] = cycler(color=sns.color_palette('colorblind'))
@@ -25,6 +28,22 @@ for ii, x in enumerate(plt.rcParams['axes.prop_cycle']()):
         break  # after 8 it repeats (for ever)
 color_dict_stand[10] = '#994F00'
 color_dict_stand[11] = '#4B0092'
+
+with open('lc_colour_mapping.json', 'r') as f:
+    lc_colour_mapping_inds = json.load(f, object_hook=lambda d: {int(k) if k.lstrip('-').isdigit() else k: v for k, v in d.items()})  # mapping from class ind to colour hex
+
+dict_ind_to_name, dict_name_to_ind = lca.get_lc_mapping_inds_names_dicts()
+lc_colour_mapping_names = {dict_ind_to_name[k]: v for k, v in lc_colour_mapping_inds.items() if k in dict_ind_to_name.keys()}
+
+def generate_list_random_colours(n):
+    list_colours =  ["#%06x" % random.randint(0, 0xFFFFFF) for _ in range(n)]
+    """
+    tmp = lcv.generate_list_random_colours(40)
+    color_dict = {x: tmp[x] for x in range(40)}
+    with open('lc_color_mapping.json', 'w') as f:
+        json.dump(color_dict, f, indent=4)
+    """
+    return list_colours
 
 ## Some generic plotting utility functions:
 def despine(ax):
@@ -102,4 +121,31 @@ def plot_image_simple(im, ax=None):
     rasterio.plot.show(plot_im, ax=ax)
     naked(ax)
 
+def plot_lc_from_gdf_dict(df_pols_tiles, tile_name='SK0066', 
+                          col_name='LC_D_80', ax=None, leg_box=(-.1, 1.05)):
+    if ax is None:
+        ax = plt.subplot(111)
+
+    df_tile = df_pols_tiles[tile_name]
+    list_colours_tile = [lc_colour_mapping_names[name] for name in df_tile['LC_D_80']]
     
+    ax = df_tile.plot(column=col_name, 
+                legend=True, linewidth=0.4, ax=ax,
+                color=list_colours_tile, edgecolor='k')
+
+    ## Create legend:
+    list_patches = []
+    for i_class, class_name in enumerate(df_tile[col_name].unique()):
+        leg_patch = mpatches.Patch(facecolor=lc_colour_mapping_names[class_name],
+                                edgecolor='k',linewidth=0.4,
+                                label=class_name)
+        list_patches.append(leg_patch)
+
+    ax.legend(handles=list_patches,  
+        title="Legend", bbox_to_anchor=leg_box,
+        fontsize=10, frameon=False, ncol=1)
+
+    naked(ax)
+    ax.set_title(f'Land cover of tile {tile_name}')
+    return ax
+        
