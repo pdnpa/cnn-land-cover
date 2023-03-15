a96d3ce10dcd3ed3b04e2bb7b31d750c563d1d94
Thijs <vdplasthijs@gmail.com>
2023-03-15 16:32:34 +0000

    new mapping dict for all low level classes

--- scripts/land_cover_analysis.py
+++ scripts/land_cover_analysis.py
@@ -1164,6 +1164,8 @@ def create_new_label_mapping_dict(mapping_type='identity', save_folder='/home/tp
             list_stay = [20, 21, 22] # these classes stay the same, everything else goes ot no-class. 
         elif mapping_type == 'E_subclasses_and_F3d_only':
             list_stay = [20, 21, 22, 28] # these classes stay the same, everything else goes ot no-class. 
+        elif mapping_type == 'all_relevant_subclasses':
+            list_stay = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 25, 28] # these classes stay the same, everything else goes ot no-class.
         else:
             raise ValueError(f'Unknown mapping type {mapping_type}')
 
